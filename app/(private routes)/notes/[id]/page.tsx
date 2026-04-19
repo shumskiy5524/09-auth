566f25dce4cbf5617a401b24a9a749e69ccf2add
shumskiy <shumskiparents@gmail.com>
2026-04-19 14:12:10 +0200

    page

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -6,6 +6,8 @@ import { useDebounce } from "use-debounce";
 import { useParams } from "next/navigation";
 import Link from "next/link";
 import { fetchNotes } from "@/lib/api/clientApi";
+import type { Note } from "@/types/note";
+
 import SearchBox from "@/components/SearchBox/SearchBox";
 import NoteList from "@/components/NoteList/NoteList";
 import Pagination from "@/components/Pagination/Pagination";
@@ -18,38 +20,44 @@ export default function Notes() {
   const [page, setPage] = useState(1);
   const [debouncedSearch] = useDebounce(search, 500);
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery<Note[]>({
     queryKey: ["notes", debouncedSearch, page, tagFromUrl],
     queryFn: () =>
       fetchNotes({
         search: debouncedSearch,
         page,
         perPage: 12,
-        tag: tagFromUrl === "all" ? "" : tagFromUrl,
+        tag: tagFromUrl === "all" ? undefined : tagFromUrl,
       }),
   });
 
-  const notes = data?.notes ?? [];
-  const totalPages = data?.totalPages ?? 0;
+  const notes = data ?? [];
 
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error loading notes</p>;
 
   return (
     <div>
-      <SearchBox value={search} onChange={(val) => { setSearch(val); setPage(1); }} />
+      <SearchBox
+        value={search}
+        onChange={(val) => {
+          setSearch(val);
+          setPage(1);
+        }}
+      />
+
       <Link href="/notes/action/create">Create note</Link>
 
       {notes.length > 0 ? (
         <>
           <NoteList notes={notes} />
-          {totalPages > 1 && (
-            <Pagination
-              currentPage={page}
-              totalPages={totalPages} 
-              onPageChange={(newPage) => setPage(newPage)}
-            />
-          )}
+
+
+          <Pagination
+            currentPage={page}
+            totalPages={1}
+            onPageChange={(newPage) => setPage(newPage)}
+          />
         </>
       ) : (
         <p>No notes found</p>
